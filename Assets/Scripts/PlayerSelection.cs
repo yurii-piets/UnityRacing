52d11fd21fa22378d26df6474579a8125106420b
Yurii Piets <yurii.piets@gmail.com>
2019-05-26 21:28:55 +0200

    add car selection menu

---
+++ Assets/Scripts/PlayerSelection.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public static class PlayerSelection {
+
+    private static Scene selectedScene
+    {
+        get { return selectedScene; }
+        set { selectedScene = value; }
+    }
+
+    private static int selectedSceneId
+    {
+        get { return selectedSceneId; }
+        set { selectedSceneId = value; }
+    }
+    
+    private static GameObject selectedCar
+    {
+        get { return selectedCar; }
+        set { selectedCar = value; }
+    } 
+    
+    private static int selectedCarId
+    {
+        get { return selectedCarId; }
+        set { selectedCarId = value; }
+    } 
+}
