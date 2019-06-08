2311879cb27097ac3f959d471574047ed30f2e44
Yurii Piets <yurii.piets@gmail.com>
2019-06-09 01:26:11 +0200

    add car engine

--- Assets/Scripts/MapSettings.cs
+++ Assets/Scripts/MapSettings.cs
@@ -4,7 +4,7 @@ class MapSettings
 {
     public static MapSettings[] mapSettings =
     {
-        new MapSettings("race_track_lake", new Vector3(-3.304075f, 2.55f, 128.0396f), Quaternion.identity),
+        new MapSettings("race_track_lake", new Vector3(-3.304075f, 2.55f, 128.0396f), Quaternion.AngleAxis(55, new Vector3(0, 1, 0))),
         new MapSettings("scene1-night", new Vector3(-146.63f, 5.63f, 160.63f), Quaternion.AngleAxis(35, new Vector3(0, 1, 0))),
         new MapSettings("sprint_track", new Vector3(953.47f, 219.96f, 924.5f), Quaternion.AngleAxis(90, new Vector3(0, 1, 0)))
     };
